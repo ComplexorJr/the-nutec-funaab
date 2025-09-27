97bd07a4f4e8e738e689f103c4ea0168fbcbd999
ComplexorJr <aniketsingh821305@gmail.com>
2025-09-28 00:52:57 +0530

    Fix React imports to resolve createContext error
    - Added React import to main.tsx and App.tsx
    - Fixed TypeError: Cannot read properties of undefined (reading 'createContext')

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { ThemeProvider } from "./components/ThemeProvider";
 import { Navigation } from "./components/Navigation";
 import { ScrollProgress } from "./components/ScrollProgress";
